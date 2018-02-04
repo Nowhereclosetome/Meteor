6dcfe6868248c9e2421f1bc60f3eb5fcf3ac2c9b
Valeri <valerisergeev98@gmail.com>
2018-02-04 21:15:20 +0300

    UsersFilter

--- client/layout/CardsTemptlate/ListStudent/Users.js
+++ client/layout/CardsTemptlate/ListStudent/Users.js
@@ -3,11 +3,30 @@ Template.UsersList.onCreated(function functionName() {
     this.subscribe('allUsers');
   });
 });
+Template.UsersList.events({
+    'click #submit': function(e,t){
+        e.preventDefault();
+        Session.set('courseSession', $('#courseSelect').val());
+        Session.set('groupSession', $('#groupSelect').val());
+    }
+});
+
 Template.UsersList.helpers({
-  users: function() {
-    return Meteor.users.find();
+    results: function(){
+      if (Session.get('courseSession') === 'все'){
+        options='<option>-</option>';
+          $("#groupSelect").html(options);
+        return Meteor.users.find();
+      }
+      else{
+        return Meteor.users.find({
+          'profile.course': Session.get('courseSession'),
+          'profile.group': Session.get('groupSession')
+        });
+      }
+
   },
   userEmail: function() {
     return this.emails[0].address;
   }
-})
+});
--- client/layout/CardsTemptlate/RegStudent/register.js
+++ client/layout/CardsTemptlate/RegStudent/register.js
@@ -4,17 +4,18 @@ Template.RegStudent.events({
     var email = t.find('#login-email').value,
         username = t.find('#username').value,
         password = t.find('#password').value,
-        course = t.find('#course').value,
-        group = t.find('#group').value,
+        course = t.find('#courseReg').value,
+        group = t.find('#groupReg').value,
         role = t.find('#role').value;
         if(role=='administrator'){
           Meteor.call('createNewUser',{username:username,password:password,email:email,profile:{course:'none',group:'teacher',role:role}});
         }
         else{
           console.log('student');
-          course = t.find('#course').value,
-          group = t.find('#group').value,
+          course = t.find('#courseReg').value,
+          group = t.find('#groupReg').value,
           role = t.find('#role').value;
+          console.log(course);
           Meteor.call('createNewUser',{username:username,password:password,email:email,profile:{course:course,group:group,role:role}});
         }
 
